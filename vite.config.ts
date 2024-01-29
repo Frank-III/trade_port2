06a6cc4f5d7608e72fb89581bbd068d778e1e962
Frank Wang <frankwaang@outlook.com>
2024-01-28 21:42:31 -0600

    chore: small tweak

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig(() => {
-  return {
-    plugins: [UnoCSS(), solid({ ssr: true })],
-    ssr: {
-      noExternal: ["@kobalte/core", "@internationalized/message"],
-    },
-  };
+	return {
+		plugins: [UnoCSS(), solid({ ssr: true, prerenderRoutes: [] })],
+		ssr: {
+			noExternal: ["@kobalte/core", "@internationalized/message"],
+		},
+	};
 });
